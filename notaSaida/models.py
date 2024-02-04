022a88b5b9d0d0c1d0260d6c0d1ac42121b4b0db
Marcello <marcellolopesdeab18@gmail.com>
2024-02-03 21:50:12 -0300

    Initial commit

---
+++ notaSaida/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from produto.models import Produtos
+
+class NotasEntradas(models.Model):
+    produto = models.ForeignKey(Produtos, on_delete=models.PROTECT)
+    quantidade = models.IntegerField('Quantidade', default=0)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=12)
+
+    criado = models.DateTimeField('Criado em', auto_now_add=True)
+    modificado = models.DateTimeField('Modificado', auto_now=True)
+
+    def __str__(self):
+        return self.produto.produto
+
+    class Meta:
+        verbose_name = 'Saidas'
+        verbose_name_plural = 'Saidas'
+        ordering = ['-produto']
+# Create your models here.
